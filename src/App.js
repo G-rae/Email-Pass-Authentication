7b33934fa3b3b5ccf00f0740ecc0cfe1b5919be5
Grae Penat <penat.grae345@gmail.com>
2024-04-02 00:44:46 +0800

    First Commit
    Data: 02/04/2024

---
+++ src/App.js
@@ -0,0 +1,77 @@
+import { useState } from "react";
+import "./App.css";
+import binignitLogo from "./img/PANDA.jpg";
+
+function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+
+  const validateInputs = () => {
+    const regExEmail = /[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,8}(.[a-z{2,8}])?/g;
+    const regExSpecialChar = /[^A-Za-z0-9]/;
+    const regExNumber = /[0-9]/;
+
+    let errorMessage = "";
+
+    if (!regExEmail.test(email)) {
+      errorMessage = "Invalid Email";
+    } else if (password && password.length < 8) {
+      errorMessage = "Password must be at least 8 characters long";
+    } else if (password && !regExSpecialChar.test(password)) {
+      errorMessage = "Password must contain at least one special character";
+    } else if (password && !regExNumber.test(password)) {
+      errorMessage = "Password must contain at least one number";
+    } else {
+      errorMessage = "Inputs are valid!";
+    }
+
+    setMessage(errorMessage);
+  };
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  return (
+    <div className="app">
+      <div className="header">
+        <h2>email & password AUTHENTICATION</h2>
+        <img src={binignitLogo} alt="Binignit Logo" className="logo" />
+      </div>
+      <div className="card">
+        <label htmlFor="email" className="label">
+          Email:
+        </label>
+        <input
+          id="email"
+          className="input"
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={handleEmailChange}
+        />
+        <label htmlFor="password" className="label">
+          Password:
+        </label>
+        <input
+          id="password"
+          className="input"
+          type="password"
+          placeholder="Password (min 8 characters, at least 1 special character, and 1 number)"
+          value={password}
+          onChange={handlePasswordChange}
+        />
+        <button className="button" onClick={validateInputs}>Check</button>
+        <p className="message">{message}</p>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
